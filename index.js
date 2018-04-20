96d690f2e896dd2ddf0f8fe8676f18ae5c54815c
dontehightower <dontedhightower@gmail.com>
2018-04-20 08:11:41 -0500

    improve code documentation

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-// This file creates the express server, serves the
-// static HTML file in the public directory, and
-// establishes the server-side websocket. 
+// This file creates the express server, serves the static HTML
+// file, located in the public directory, and establishes the
+// server-side websocket.
 
-// The client-side websocket is established in the
-// /public/chat.js file
+// The client-side websocket is established in the /public/chat.js
+// file. And linked to the /public/index.HTML file via a script tag
+// in the body.
 
 const express = require('express');
 const socket = require('socket.io');
@@ -18,9 +19,7 @@ app.use(express.static('public'));
 
 // Socket setup
 const io = socket(server);
-
-
 // When the socket connects, fire a callback function
 io.on('connection', function(socket) {
   console.log('Socket connected to server', socket.id);
-})
+});
--- public/chat.js
+++ public/chat.js
@@ -1,7 +1,4 @@
-// This file establishes a connection to the server
-// to create a websocket between the server and the
-// static HTML page. The connection made in this file
-// is the connection being listened for in the index.js
-// server file.
-
+// This file establishes a connection to the server to create a socket
+// between the server and the index.html page. The connection made in this file
+// is the connection being listened for in the server file, titled index.js.
 const socket = io.connect('http://localhost:4000');
